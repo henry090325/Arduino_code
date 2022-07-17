2e995a5fa97afea9d6a157cd4814d030ab6895de
hwiseong.shin <henry090325@gmail.com>
2022-07-17 17:02:12 +0900

    LED 3 on&off
    1.0.1

--- sketch_jul17c/LED_code/LED_code.ino
+++ sketch_jul17c/LED_code/LED_code.ino
@@ -1,11 +1,20 @@
 void setup() {
   pinMode(7, OUTPUT);
-
+  pinMode(8, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 void loop() {
   digitalWrite(7, HIGH);
   delay(1000);
   digitalWrite(7, LOW);
+  delay(1000);
+  digitalWrite(8, HIGH);
+  delay(1000);
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(11, HIGH);
+  delay(1000);
+  digitalWrite(11, LOW);
   delay(1000);
 }
