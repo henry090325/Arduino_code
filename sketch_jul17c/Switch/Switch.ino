33549146746c2daeabad506f9276b2de579d0a89
hwiseong.shin <henry090325@gmail.com>
2022-07-17 18:52:29 +0900

    INPUT_PULLUP
    1.0.1

--- sketch_jul17c/Switch/Switch.ino
+++ sketch_jul17c/Switch/Switch.ino
@@ -1,9 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(13,INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(13)==HIGH){
+    Serial.println("1");
+  }else{
+    Serial.println("0");
+  }
 }
+//●▅▇█▇▆▅▄▇
