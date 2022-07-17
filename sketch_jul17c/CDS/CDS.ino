5dd14ef4da96145e77a18b4a944935c5e7d1b969
hwiseong.shin <henry090325@gmail.com>
2022-07-17 18:23:51 +0900

    CDS
    1.0.1

--- sketch_jul17c/CDS/CDS.ino
+++ sketch_jul17c/CDS/CDS.ino
@@ -1,9 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int lightValue = analogRead(A0);
 
+  Serial.println(lightValue);
+  if(lightValue < 300) {
+    digitalWrite(7, HIGH);
+  }else {
+    digitalWrite(7, LOW);
+  }
 }
