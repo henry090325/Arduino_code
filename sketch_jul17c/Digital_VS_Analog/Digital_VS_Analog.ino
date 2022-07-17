c3c05a113a17aa66d8b8895679a4b86d880d3467
hwiseong.shin <henry090325@gmail.com>
2022-07-17 17:44:31 +0900

    Analog
    1.0.1

--- sketch_jul17c/Digital_VS_Analog/Digital_VS_Analog.ino
+++ sketch_jul17c/Digital_VS_Analog/Digital_VS_Analog.ino
@@ -5,5 +5,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogWrite(3,255);
+  for(int i = 0;i<256; i++){
+    analogWrite(3, i);
+    delay(10);
+  }
+  for(int i = 255;i>0; i--){
+    analogWrite(3, i);
+    delay(10);
+  }
 
 }
