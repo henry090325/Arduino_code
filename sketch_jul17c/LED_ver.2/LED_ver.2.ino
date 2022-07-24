4cf1213e9cc954af25de3e181951fcd854b80177
hwiseong.shin <henry090325@gmail.com>
2022-07-24 18:48:51 +0900

    Update LED_ver.2.ino

--- sketch_jul17c/LED_ver.2/LED_ver.2.ino
+++ sketch_jul17c/LED_ver.2/LED_ver.2.ino
@@ -1,9 +1,8 @@
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  
 }
 
 void loop() {
-  int lightValue = analogRead(A0);
-  Serial.println(lightValue);
+  
 }
